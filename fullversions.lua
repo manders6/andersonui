ffdec552aee673bc09c4cff542df038b8ab0a018
andersonma <manders6@lakeheadu.ca>
2020-08-03 10:51:29 -0400

    Added in Installer Packages Added fullversions.lua

---
+++ fullversions.lua
@@ -0,0 +1,9 @@
+return {{[1] = {2},[2] = {3},},
+{[ [[version]] ] = [[0.1.0.0]],[ [[notes]] ] = [[]],},
+{[ [[version]] ] = [[0.2.0.0]],[ [[notes]] ] = [[]],},
+{[ [[version]] ] = [[0.2.0.1]],[ [[notes]] ] = [[]],},
+{[ [[version]] ] = [[0.2.0.2]],[ [[notes]] ] = [[]],},
+{[ [[version]] ] = [[0.2.1.0]],[ [[notes]] ] = [[]],},
+{[ [[version]] ] = [[0.2.1.1]],[ [[notes]] ] = [[]],},
+{[ [[version]] ] = [[0.2.1.2]],[ [[notes]] ] = [[Added debuff bar\nAdded multiple settings options\nCleaned up UI]],},
+}
