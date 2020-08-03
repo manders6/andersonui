b45b376dd597d44f524be3a2fbb30039e6d8548e
andersonma <manders6@lakeheadu.ca>
2020-08-03 11:16:09 -0400

    Fullversion fix

--- fullversions.lua
+++ fullversions.lua
@@ -1,9 +1,11 @@
-return {{[1] = {2},[2] = {3},},
+return {{[1] = {2},[2] = {3},[3] = {4},[4] = {5},[5] = {6},[6] = {7},[7] = {8},},
 {[ [[version]] ] = [[0.1.0.0]],[ [[notes]] ] = [[]],},
 {[ [[version]] ] = [[0.2.0.0]],[ [[notes]] ] = [[]],},
 {[ [[version]] ] = [[0.2.0.1]],[ [[notes]] ] = [[]],},
 {[ [[version]] ] = [[0.2.0.2]],[ [[notes]] ] = [[]],},
 {[ [[version]] ] = [[0.2.1.0]],[ [[notes]] ] = [[]],},
 {[ [[version]] ] = [[0.2.1.1]],[ [[notes]] ] = [[]],},
-{[ [[version]] ] = [[0.2.1.2]],[ [[notes]] ] = [[Added debuff bar\nAdded multiple settings options\nCleaned up UI]],},
+{[ [[version]] ] = [[0.2.0.2]],[ [[notes]] ] = [[Added debuff bar
+Added multiple settings options
+Cleaned up UI]],},
 }
